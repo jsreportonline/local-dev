950c95a75234e2327be8c7ee43dfec88b6c77d39
BJR Matos <bjrmatos@gmail.com>
2018-06-20 15:38:41 -0500

    update config values and compose

--- clone-all.bat
+++ clone-all.bat
@@ -1,11 +1,4 @@
 cd ..
 git clone https://github.com/jsreportonline/main.git
-git clone https://github.com/jsreportonline/workers.git
-git clone https://github.com/jsreportonline/tasks.git
 git clone https://github.com/jsreportonline/sales.git
-git clone https://github.com/jsreportonline/wkhtmltopdf.git
-git clone https://github.com/jsreportonline/fop-pdf.git
 git clone https://github.com/jsreportonline/windows-workers.git
-git clone https://github.com/jsreportonline/electron-pdf.git
-git clone https://github.com/jsreportonline/html-to-xlsx.git
-git clone https://github.com/jsreportonline/phantom-pdf.git
